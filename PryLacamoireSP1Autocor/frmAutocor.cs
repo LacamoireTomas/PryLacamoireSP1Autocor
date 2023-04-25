457d78915adf34efe912d4fdc13321c80c905dbe
LacamoireTomas <tomaslacamoire@gmail.com>
2023-04-24 22:16:06 -0300

    Agregar archivos de proyecto.

---
+++ PryLacamoireSP1Autocor/frmAutocor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PryLacamoireSP1Autocor
+{
+    public partial class frmAutocor : Form
+    {
+        public frmAutocor()
+        {
+            InitializeComponent();
+        }
+    }
+}
